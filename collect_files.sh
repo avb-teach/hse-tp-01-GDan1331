5e25fbc513c0d814bbc96b66b14742f6679166c1
GDan1331 <fastlightning519@gmail.com>
2025-04-30 11:57:55 +0300

    v5

--- collect_files.sh
+++ collect_files.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ $3 == "--max_depth" ]]; then
-    python3 main.py $1 $2 $5
+    python3 main.py $1 $2 $4
 else
     python3 main.py $1 $2 0
 fi
--- main.py
+++ main.py
@@ -5,16 +5,15 @@ input_dir = sys.argv[1]
 output_dir = sys.argv[2]
 max_depth = sys.argv[3]
 max_depth = int(max_depth)
-if max_depth != 0:
-    max_depth -= 1
 for i in os.walk(input_dir):
     pth = i[0].split('/')[1:]
-    depth_path = pth[len(pth) - max_depth:]
+    depth_path = pth[len(pth) - max_depth + 1:]
     create = output_dir + "/"
     for j in depth_path:
         create += j + "/"
         if not os.path.isdir(create):
             os.system(f"mkdir {create}")
+
     for file in i[2]:
         os.system(
                 f'cp {i[0]}/{file} {create}'
