e6eb207cdd84411e3e17901e25ec81f10ffc6188
GDan1331 <fastlightning519@gmail.com>
2025-04-30 12:32:08 +0300

    v8

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ max_depth = sys.argv[3]
 max_depth = int(max_depth) - 1
 for i in os.walk(input_dir):
     pth = i[0].split('/')[1:]
-    depth_path = pth[max(0, len(pth) - max_depth):]
     create = output_dir + "/"
-    for j in depth_path:
+    for j in pth[max(0, len(pth) - max_depth):]:
         create += j + "/"
         if not os.path.isdir(create):
             os.system(f"mkdir {create}")
