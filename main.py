c7e73661db14acc07247d7b111c07f4de6c32001
GDan1331 <fastlightning519@gmail.com>
2025-04-30 12:39:14 +0300

    v9

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ output_dir = sys.argv[2]
 max_depth = sys.argv[3]
 max_depth = int(max_depth) - 1
 for i in os.walk(input_dir):
-    pth = i[0].split('/')[1:]
-    create = output_dir + "/"
-    for j in pth[max(0, len(pth) - max_depth):]:
-        create += j + "/"
-        if not os.path.isdir(create):
-            os.system(f"mkdir {create}")
-
-    for file in i[2]:
-        os.system(
-                f'cp {i[0]}/{file} {create}'
-            )
+    if i[2]:
+        pth = i[0].split('/')[1:]
+        create = output_dir + "/"
+        for j in pth[max(0, len(pth) - max_depth):]:
+            create += j + "/"
+            if not os.path.isdir(create):
+                os.system(f"mkdir {create}")
+        for file in i[2]:
+            os.system(
+                    f'cp {i[0]}/{file} {create}'
+                )
